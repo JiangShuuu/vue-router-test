20417f1c66cc85266d4c7826b67b071d51240a2c
JiangShuuu <globelex65@gmail.com>
2022-10-10 21:48:12 +0800

    add breadCrumb

---
+++ src/components/breadCrumb.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="bread">
+    <li v-for="(item, i) in breadcrumbList" :key="i" class="bread-contain">
+      <router-link :to="item.path">{{ item.meta.title }}</router-link>
+    </li>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const breadcrumbList = ref()
+
+watch(route, () => {
+  getBreadcrumbs()
+})
+onMounted(() => {
+  getBreadcrumbs()
+})
+
+const getBreadcrumbs = () => {
+  let matched: any = route.matched
+
+  matched = [{ path: '/', meta: { title: '首頁' } }].concat(matched)
+
+  breadcrumbList.value = matched
+}
+
+</script>
+
+<style scoped>
+.bread {
+  display: flex;
+  align-items: center;
+  margin: 20px 12px;
+  color: grey;
+}
+
+.bread-contain {
+  margin: 0 8px 0 0;
+}
+
+li {
+  list-style: none;
+}
+
+li::after {
+  content: '/';
+  margin: 0 0 0 12px;
+}
+
+li:last-child::after {
+  content: '';
+}
+</style>
--- src/views/member.vue
+++ src/views/member.vue
@@ -1,4 +1,9 @@
 <template>
   <h1>Member Page</h1>
+  <breadCrumb />
   <router-view />
 </template>
+
+<script setup lang="ts">
+import breadCrumb from '../components/breadCrumb.vue'
+</script>
