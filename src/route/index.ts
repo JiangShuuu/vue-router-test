a8d6cc17439ae98e395d0fb03cfd0c7d9d19f3fd
JiangShuuu <globelex65@gmail.com>
2022-10-12 22:51:53 +0800

    upd

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(),
-  linkActiveClass: 'vue-active-link',
+  linkExactActiveClass: 'vue-active-link',
   routes: [
     {
       path: '/',
