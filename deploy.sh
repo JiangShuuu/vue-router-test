909493b0a5074cfda78a52bb00a1d6fc98e73bec
JiangShuuu <globelex65@gmail.com>
2022-10-15 11:29:07 +0800

    add dist

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:JiangShuuu/vue3-test-website.git main:gh-pages
+git push -f git@github.com:JiangShuuu/vue-router-test.git main:gh-pages
 
 cd -
